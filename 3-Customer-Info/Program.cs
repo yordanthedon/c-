1c6e8a0292c0a3500bfa29d82d597cb5d1721546
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-10 10:46:35 +0200

    Add files via upload

---
+++ 3-Customer-Info/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string email = Console.ReadLine();
+
+Console.WriteLine($"Customer: {firstName} {lastName} ({email})");
---
+++ 4-Person-Info/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string country = Console.ReadLine();
+string town = Console.ReadLine();
+
+Console.WriteLine($"{firstName} {lastName} from {country} - {town}!");
