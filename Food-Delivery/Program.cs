7bafd5975504229208d98b5bf9e20a0bd5d73e76
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-18 12:42:51 +0200

    Add files via upload

---
+++ Food-Delivery/Program.cs
@@ -0,0 +1,14 @@
+﻿int chiken = int.Parse(Console.ReadLine());
+int fish = int.Parse(Console.ReadLine());
+int vegetarian = int.Parse(Console.ReadLine());
+
+double delivery = 2.50;
+
+double chikenMenus = chiken * 10.35;
+double fishMenus = fish * 12.40;
+double vegetarianMenus = vegetarian * 8.15;
+double totalMenus =  chikenMenus + fishMenus + vegetarianMenus;
+double dessert = totalMenus * 0.2;
+double totalOrder = totalMenus + dessert + delivery;
+
+Console.WriteLine(totalOrder);
---
+++ Redecorating/Program.cs
@@ -0,0 +1,15 @@
+﻿int nylon = int.Parse(Console.ReadLine());
+int paint = int.Parse(Console.ReadLine());
+int thinner = int.Parse(Console.ReadLine());
+int hoursWork = int.Parse(Console.ReadLine());
+
+double bags = 0.40;
+
+double nylonAmount = (nylon + 2) * 1.50;
+double paintAmount = (paint + (paint * 0.1)) * 14.50;
+double thinnerAmount = thinner * 5;
+double totalMaterials = nylonAmount + paintAmount + thinnerAmount + bags;
+double craftsmen = (totalMaterials * 0.3) * hoursWork;
+double total = totalMaterials + craftsmen;
+
+Console.WriteLine(total);
