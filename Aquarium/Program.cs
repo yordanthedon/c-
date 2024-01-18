cbe016b4d403044ab26c0b69671c54056de69be1
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-18 12:41:25 +0200

    Add files via upload

---
+++ Aquarium/Program.cs
@@ -0,0 +1,12 @@
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int heigth = int.Parse(Console.ReadLine());
+double percentage = double.Parse(Console.ReadLine());
+
+double realPerc = percentage / 100.0;
+
+double volumeAcuarium = length * width * heigth;
+double volumeLiters = volumeAcuarium / 1000;
+double requiredLiters = volumeLiters * (1 - realPerc);
+
+Console.WriteLine($"{requiredLiters:F2}");
---
+++ Basketball-Equipment/Program.cs
@@ -0,0 +1,9 @@
+﻿int fee = int.Parse(Console.ReadLine());
+
+double sneakers = fee - (fee * 0.4);
+double team = sneakers - (sneakers * 0.2);
+double basketball = team * 0.25;
+double accessories = basketball * 0.2;
+double total = fee + sneakers + team + basketball + accessories;
+
+Console.WriteLine(total);
