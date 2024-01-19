3833bca87cc803531c552e1ef28d90f5480a56e8
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-19 08:37:32 +0200

    Add files via upload

---
+++ Centuries-to-Minutes/Program.cs
@@ -0,0 +1,8 @@
+﻿int centuries = int.Parse(Console.ReadLine());
+
+int years = centuries * 100;
+double days = Math.Floor(years * 365.2422);
+double hours = days * 24;
+double minutes = hours * 60;
+
+Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
---
+++ Reversed-Numbers/Program.cs
@@ -0,0 +1,7 @@
+﻿double one = double.Parse(Console.ReadLine());
+double two = double.Parse(Console.ReadLine());
+double three = double.Parse(Console.ReadLine());
+
+Console.WriteLine(three);
+Console.WriteLine(two);
+Console.WriteLine(one);
