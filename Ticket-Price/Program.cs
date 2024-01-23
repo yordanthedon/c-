5b25300ade1a55f525f84d33a3c9f2cd31caa32a
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-23 14:18:51 +0200

    Add files via upload

---
+++ Ticket-Price/Program.cs
@@ -0,0 +1,14 @@
+﻿string ticket = Console.ReadLine();
+
+if (ticket == "student")
+{
+    Console.WriteLine("$1.00");
+}
+else if (ticket == "regular")
+{
+    Console.WriteLine($"$1.60");
+}
+else
+{
+    Console.WriteLine("Invalid ticket type!");
+}
---
+++ Valid-Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿int side1 = int.Parse(Console.ReadLine());
+int side2 = int.Parse(Console.ReadLine());
+int side3 = int.Parse(Console.ReadLine());
+
+if (side1 >= side2 + side3)
+{
+    Console.WriteLine("Invalid Triangle");
+}
+else if (side2 >= side1 + side3)
+{
+    Console.WriteLine("Invalid Triangle");
+}
+else if (side3 >= side1 + side2)
+{
+    Console.WriteLine("Invalid Triangle");
+}
+else
+{
+    Console.WriteLine("Valid Triangle");
+}
