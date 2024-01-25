7faa6ab0bb225b30b1fe162af87b66aca5ce6f87
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-25 21:46:47 +0200

    Add files via upload

---
+++ Sum-Seconds/Program.cs
@@ -0,0 +1,10 @@
+﻿int firstTime = int.Parse(Console.ReadLine());
+int secondTime = int.Parse(Console.ReadLine());
+int thirdTime = int.Parse(Console.ReadLine());
+
+int sumSec = firstTime + secondTime + thirdTime;
+
+int sumMin = sumSec / 60;
+int sumMins = sumSec % 60;
+
+Console.WriteLine($"{sumMin}:{sumMins:D2}");
---
+++ Vacation-Expenses/Program.cs
@@ -0,0 +1,51 @@
+﻿string season = Console.ReadLine();
+string type = Console.ReadLine();
+int days = int.Parse(Console.ReadLine());
+
+double totalExpen = 0;
+
+if (season == "Spring")
+{
+    if (type == "Hotel")
+    {
+        totalExpen = days * 30 - (days * 30 * 0.2);
+    }
+    else if (type == "Camping")
+    {
+        totalExpen = days * 10 - (days * 10 * 0.2);
+    }
+}
+else if (season == "Summer")
+{
+    if (type == "Hotel")
+    {
+        totalExpen = days * 50;
+    }
+    else if (type == "Camping")
+    {
+        totalExpen = days * 30;
+    }
+}
+else if (season == "Autumn")
+{
+    if (type == "Hotel")
+    {
+        totalExpen = days * 20 - (days * 20 * 0.3);
+    }
+    else if (type == "Camping")
+    {
+        totalExpen = days * 15 - (days * 15 * 0.3);
+    }
+}
+else if (season == "Winter")
+{
+    if (type == "Hotel")
+    {
+        totalExpen = days * 40 - (days * 40 * 0.1);
+    }    
+    else if (type == "Camping")
+    {
+        totalExpen = days * 10 - (days * 10 * 0.1);
+    }    
+}
+Console.WriteLine($"{totalExpen:F2}");
