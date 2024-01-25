639ecd27d35791e5e506ec6e892eb0da23444d48
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-25 21:41:22 +0200

    Add files via upload

---
+++ Bonus-Score/Program.cs
@@ -0,0 +1,17 @@
+﻿int points = int.Parse(Console.ReadLine());
+
+int bonus = 0;
+
+if (points >= 0 && points <= 3)
+{
+    bonus = points + 5;
+}
+else if (points >= 4 && points <= 6)
+{
+    bonus = points + 15;
+}
+else if (points >= 7 && points <= 9)
+{
+    bonus = points + 20;
+}
+Console.WriteLine(bonus);
---
+++ Cinema/Program.cs
@@ -0,0 +1,21 @@
+﻿string type = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int seats = int.Parse(Console.ReadLine());
+
+double ticketPrice = 0;
+
+if (type == "Premiere")
+{
+    ticketPrice = 12.00;
+}
+else if (type == "Normal")
+{
+    ticketPrice = 7.50;
+}
+else if (type == "Discount")
+{
+    ticketPrice = 5.00;
+}
+
+double totalPrice = rows * seats * ticketPrice;
+Console.WriteLine($"{totalPrice:F2}");
