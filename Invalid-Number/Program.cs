e6c836b39f6ef7e0170be0a158b9d91892cb2fa6
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-25 21:42:35 +0200

    Add files via upload

---
+++ Invalid-Number/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+bool isValid = (number >= 100 && number <= 200) || number == 0;
+
+if (!isValid)
+{
+    Console.WriteLine("invalid");
+}
---
+++ Largest-Number-Out-of-Three/Program.cs
@@ -0,0 +1,22 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+        Console.WriteLine(num1);
+    else
+    {
+        Console.WriteLine(num3);
+    }
+}
+else 
+{
+    if (num2 > num3)
+        Console.WriteLine(num2);
+    else
+    { 
+        Console.WriteLine(num3);
+    }
+}
