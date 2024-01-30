9fe9fe40657a60d5ad9013e09dd26b22eabf0a51
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-01-30 15:19:37 +0200

    Add files via upload

---
+++ Numbers-in-Range/Program.cs
@@ -0,0 +1,7 @@
+﻿int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+
+for (int i = first; i <= second; i++)
+{
+    Console.WriteLine(i);
+}
---
+++ Sum-Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+double sum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    double number = double.Parse(Console.ReadLine());
+    sum += number;
+}
+
+Console.WriteLine(sum);
