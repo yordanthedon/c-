3a085a8ce01aeeacb974abd0f50ebc6cc78b2116
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-01 11:18:30 +0200

    Add files via upload

---
+++ Number-Processor/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string word = word = Console.ReadLine();
+
+while (word != "End")
+{
+    if (word == "Inc")
+    {
+        n++;
+    }
+    else if (word == "Dec")
+    {
+        n--;
+    }
+    word = Console.ReadLine();
+}
+Console.WriteLine(n);
---
+++ Number-in-Range/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+bool isValid = number >= 1 && number <= 100;
+
+while (!isValid)
+{
+    number = int.Parse(Console.ReadLine());
+    isValid = number >= 1 && number <= 100;
+}
+
+Console.WriteLine(number);
