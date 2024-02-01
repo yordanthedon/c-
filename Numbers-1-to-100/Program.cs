2f8f2a3e905b04b4fe716fe93ba87a978b9793ba
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-01 11:19:23 +0200

    Add files via upload

---
+++ Numbers-1-to-100/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = 1;
+while (number <= 100)
+{
+    Console.WriteLine(number);
+    number++;
+}
---
+++ Odd-Number/Program.cs
@@ -0,0 +1,7 @@
+﻿double numbers = double.Parse(Console.ReadLine());
+
+while (numbers % 2 == 0)
+{
+   numbers = double.Parse(Console.ReadLine());
+}
+Console.WriteLine(numbers);
