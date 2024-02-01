7fd0db4540819aae5ff49ef3ea1e8728a2c9dc0a
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-01 11:20:08 +0200

    Add files via upload

---
+++ Sequence-2k+1/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int i = 1;
+while (i <= n)
+{
+    Console.WriteLine(i);
+    i = 2 * i + 1;
+}
---
+++ Sum-Digits/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (n != 0)
+{
+    int digit = n % 10;
+    n /= 10;
+    sum += digit;
+}
+Console.WriteLine(sum);
