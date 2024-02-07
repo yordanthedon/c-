16858cc4ff0c415e2e89a5e6b99abbda51dab909
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-07 22:19:20 +0200

    Add files via upload

---
+++ Prime-Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+
+for (int i = start; i <= end; i++)
+{
+    bool isPrime = true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            isPrime = false;
+        }
+    }
+    if (isPrime)
+    {
+        Console.Write(i + " ");
+    }
+}
---
+++ Sum-of-Digits-Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = Console.ReadLine();
+
+while (input != "End")
+{
+    int number = int.Parse(input);
+    int sumDigits = 0;
+
+    while (number > 0)
+    {
+        int digits = number % 10;
+        sumDigits += digits;
+        number /= 10;
+    }
+    Console.WriteLine($"Sum of digits = {sumDigits}");
+    
+    input = Console.ReadLine();
+}
+Console.WriteLine("Goodbye");
