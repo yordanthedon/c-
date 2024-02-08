60f24b97f84ea2c67ba1f0833df42ddaf9b8cdd8
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-08 22:00:51 +0200

    Add files via upload

---
+++ Orders/Program.cs
@@ -0,0 +1,27 @@
+﻿string prod = Console.ReadLine();
+int quan = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{TotalOrders(prod, quan):F2}");
+
+static double TotalOrders(string p, int q)
+{
+    double totalSum = 0;
+
+    if (p == "coffee")
+    {
+        totalSum = q * 1.50;
+    }
+    else if (p == "water")
+    {
+        totalSum = q * 1.00;
+    }
+    else if (p == "coke")
+    {
+        totalSum = q * 1.40;
+    }
+    else if (p == "snacks")
+    {
+        totalSum = q * 2.00;
+    }
+    return totalSum;
+}
---
+++ Printing-Triangle/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+PrintTriangle(n);
+static void PrintLine(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+static void PrintTriangle(int number)
+{
+    for (int i = 1; i < number; i++)
+    {
+        PrintLine(1, i);
+    }
+    for (int i = number; i >= 1; i--)
+    {
+        PrintLine(1, i);
+    }
+}
