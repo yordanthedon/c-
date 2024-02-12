39cc66891d22c4860127a3b4f3f168e80e78794d
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-12 18:19:13 +0200

    Add files via upload

---
+++ Multiplication-Sign/Program.cs
@@ -0,0 +1,23 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+NumberPositive(num1, num2, num3);
+
+static void NumberPositive(int num1, int num2, int num3)
+{
+    int totalSum = num1 * num2 * num3;
+
+    if (totalSum > 0)
+    {
+        Console.WriteLine("positive");
+    }
+    else if (totalSum < 0)
+    {
+        Console.WriteLine("negative");
+    }
+    else
+    {
+        Console.WriteLine("zero");
+    }
+}
---
+++ Password-Validator/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Testt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            IsValidPass(password);
+        }
+
+        private static void IsValidPass(string password)
+        {
+            int numberOfDigits = 0;
+            foreach (char d in password)
+            {
+                if (char.IsDigit(d))
+                {
+                    numberOfDigits++;
+                }
+            }
+            bool isLetterOrDigits = password.All(char.IsLetterOrDigit);
+
+            if ((password.Length >= 6 && password.Length <= 10) &&
+                isLetterOrDigits && numberOfDigits >= 2)
+            {
+                Console.WriteLine("Password is valid");
+                return;
+            }
+            if (!(password.Length >= 6 && password.Length <= 10))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+            if (isLetterOrDigits == false)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+            if (numberOfDigits < 2)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+        }
+    }
+}
