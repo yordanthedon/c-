3452803f9b8f56e688a94a592c4af7608db6d7f7
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-14 23:47:03 +0200

    Add files via upload

---
+++ List-of-Products/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+List<string> products = new List<string>();
+
+for  (int i = 0; i < number; i++)
+{
+    string product = Console.ReadLine();
+    
+    products.Add(product);
+}
+
+products.Sort();
+
+for (int i = 0;i < products.Count;i++)
+{
+    Console.WriteLine($"{i + 1}.{products[i]}");
+}
---
+++ Remove-Negatives-and-Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    int currentNumber = numbers[i];
+
+    if (currentNumber < 0)
+    {
+        numbers.Remove(currentNumber);
+        i--;
+    }
+}
+
+if (numbers.Count > 0)
+{
+    numbers.Reverse();
+    Console.WriteLine(string.Join(" ", numbers));
+}
+else
+{
+    Console.WriteLine("empty");
+}
