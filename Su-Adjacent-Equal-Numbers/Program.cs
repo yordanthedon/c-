4b52dea1fe4ab97847af026d247f0fcb558407b1
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-19 23:55:15 +0200

    Add files via upload

---
+++ Su-Adjacent-Equal-Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+while (true)
+{
+    bool hasEqualAdjacents = false;
+    for (int i = 0; i < numbers.Count - 1; i++)
+    {
+        int currentNumber = numbers[i];
+        int nextNumber = numbers[i + 1];
+        
+        if (currentNumber == nextNumber)
+        {
+            int sum = currentNumber + nextNumber;
+            numbers.RemoveAt(i);
+            numbers.RemoveAt(i);
+            
+            numbers.Insert(i, sum);
+
+            hasEqualAdjacents = true;
+            break;
+
+        }
+    }
+    if (!hasEqualAdjacents)
+    {
+        break;
+    }
+}
+Console.WriteLine(string.Join(" ", numbers));
---
+++ Zig-Zag-Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<int> firstArray = new List<int>();
+List<int> secondArray = new List<int>();
+            
+for (int i = 0; i < n; i++)            
+{
+    List<int> numbers = Console.ReadLine()
+                        .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToList();
+
+    if (i % 2 == 0)
+    {
+        var firstArrayNumber = numbers[0];
+        var secondArrayNumber = numbers[1];
+        firstArray.Add(firstArrayNumber);
+        secondArray.Add(secondArrayNumber);
+    }
+    else
+    {
+        var firstArrayNumber = numbers[1];
+        var secondArrayNumber = numbers[0];
+        firstArray.Add(firstArrayNumber);
+        secondArray.Add(secondArrayNumber);
+    }
+}
+
+Console.WriteLine(string.Join(" ", firstArray));
+Console.WriteLine(string.Join(" ", secondArray));
