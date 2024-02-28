e1118ad1bdaba14bc4dec15b9c98b84e2646b46c
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-02-28 10:28:59 +0200

    Add files via upload

---
+++ Middle-Elements/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int middle = numbers.Length / 2;
+
+int firstNum = numbers[middle - 1];
+int secondNum = numbers[middle];
+
+double result = (firstNum + secondNum) / 2.00;
+
+Console.WriteLine($"{result:F2}");
---
+++ Sum-Factorial-Even-Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    int digit = number % 10;
+    number /= 10;
+    
+    if (digit % 2 == 0)
+    {
+        int factoriel = 1;
+
+        for (int i = 1; i <= digit; i++)
+        {
+            factoriel *= i;
+        }
+        sum += factoriel;
+    }
+}
+
+Console.WriteLine(sum);
