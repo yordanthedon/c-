3a713d8596d47c599f6f4be893e7dec4f0c78549
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-03-03 23:16:20 +0200

    Add files via upload

---
+++ problem01/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int startNum = int.Parse(Console.ReadLine());
+int endNum = int.Parse(Console.ReadLine());
+
+int maxNumber = numbers.Skip(startNum).Take(endNum - startNum + 1).Max();
+
+int minNumber = numbers.Skip(startNum).Take(endNum - startNum + 1).Min();
+
+int sum = maxNumber + minNumber;
+
+Console.WriteLine(sum);
---
+++ problem02/Program.cs
@@ -0,0 +1,25 @@
+﻿string text = Console.ReadLine();
+
+int upperCaseCount = 0;
+int lowerCaseCount = 0;
+int spaceCount = 0;
+
+foreach (char letter in text)
+{
+    if (char.IsUpper(letter))
+    {
+        upperCaseCount++;
+    }
+    else if (char.IsLower(letter))
+    {
+        lowerCaseCount++;
+    }
+    else if (char.IsWhiteSpace(letter))
+    {
+        spaceCount++;
+    }
+}
+
+Console.WriteLine(upperCaseCount);
+Console.WriteLine(lowerCaseCount);
+Console.WriteLine(spaceCount);
