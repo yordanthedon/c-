26929adce51135b1117d61435343cac5150d9f71
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-03-05 22:08:17 +0200

    Add files via upload

---
+++ Digits-Letters-and-Other/Program.cs
@@ -0,0 +1,25 @@
+﻿string input = Console.ReadLine();
+
+string digits = "";
+string letters = "";
+string others = "";
+
+foreach (char letter in input)    
+{
+    if (char.IsLetter(letter))
+    {
+        letters += letter;
+    }
+    else if (char.IsDigit(letter))
+    {
+        digits += letter;
+    }
+    else
+    {
+        others += letter;
+    }
+}
+
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(others);
---
+++ Repeat-Strings/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] strings = Console.ReadLine().Split().ToArray();
+
+string output = "";
+
+foreach (string s in strings)
+{
+    for (int i = 0; i < s.Length; i++)
+    {
+        output += s;
+    }
+}
+
+Console.WriteLine(output);
