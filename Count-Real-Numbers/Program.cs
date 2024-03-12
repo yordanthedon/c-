b5070b01a7eb66c59ffc31486bd1a4a87ee3b313
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-03-12 22:42:42 +0200

    Add files via upload

---
+++ Count-Real-Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+SortedDictionary<double, int> counter = new SortedDictionary<double, int>();
+
+foreach (var number in numbers)
+{
+    if (counter.ContainsKey(number))
+    {
+        counter[number]++;
+    }
+    else
+    {
+        counter.Add(number, 1);
+    }
+}
+
+foreach (var number in counter)
+{
+    Console.WriteLine($"{number.Key} -> {number.Value}");
+}
