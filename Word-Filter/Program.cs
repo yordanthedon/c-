97fe7a7d4bc80a3a87fee8c28100d234ceb0c3e0
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-03-12 22:43:38 +0200

    Add files via upload

---
+++ Word-Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> strings = Console.ReadLine().Split().ToList();
+
+List<string> newList = new List<string>();
+
+foreach (string s in strings)
+{
+    if (s.Length % 2 == 0)
+    {
+        newList.Add(s);
+    }
+}
+
+foreach (string s in newList)
+{
+    Console.WriteLine(s);
+}
+
+//List<string> str = Console.ReadLine()
+//                       .Split()
+//                       .Where(s => s.Length % 2 == 0)
+//                       .Select(s => { Console.WriteLine(s); return s; })
+//                       .ToList();
