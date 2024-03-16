0e787c4eb8b3e21efa470fbc5eeb00f20c9cc5c9
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-03-16 21:36:04 +0200

    Add files via upload

---
+++ A-Miner-Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string input = Console.ReadLine();
+
+var resources = new Dictionary<string, double>();
+
+while (input != "stop")
+{
+    int quantity = int.Parse(Console.ReadLine());
+    
+    if (resources.ContainsKey(input))
+    {
+        resources[input] += quantity;
+    }
+    else
+    {
+        resources.Add(input, quantity);
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach (var pair in resources)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
---
+++ Count-Chars-in-a-String/Program.cs
@@ -0,0 +1,24 @@
+﻿string input = Console.ReadLine();
+
+var characters = new Dictionary<char, int>();
+
+foreach (var ch in input)
+{
+    if (ch == ' ')
+    {
+        continue;
+    }
+    if (characters.ContainsKey(ch))
+    {
+        characters[ch]++;
+    }
+    else
+    {
+        characters.Add(ch, 1);
+    }
+}
+
+foreach(var pair in characters)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
