c5800bf700b5b00510af1e4fabad3c582c7e082c
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-03-16 21:38:29 +0200

    Add files via upload

---
+++ Company-Users/Program.cs
@@ -0,0 +1,33 @@
+﻿var companiesWithEmpl = new Dictionary<string, List<string>>();
+
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    string[] inputParams = input.Split(" -> ");
+    string company = inputParams[0];
+    string emplName = inputParams[1];
+
+    if (companiesWithEmpl.ContainsKey(company))
+    {
+        if (!companiesWithEmpl[company].Contains(emplName))
+        {
+            companiesWithEmpl[company].Add(emplName);
+        }
+    }
+    else
+    {
+        companiesWithEmpl.Add(company, new List<string>() { emplName });
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach (var pair in companiesWithEmpl)
+{
+    Console.WriteLine(pair.Key);
+    foreach (var employee  in pair.Value)
+    {
+        Console.WriteLine($"-- {employee}");
+    }
+} 
